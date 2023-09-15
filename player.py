93d0080a56dc40d72cfd092db8fb3e4c9e6da3e2
Wanwisa Waddington <kaywanwisa4@googlemail.com>
2023-09-15 08:24:51 +0000

    Add the version after checking from CI python Linter

--- player.py
+++ player.py
@@ -2,39 +2,45 @@ import random
 import math
 from colorama import Fore, Back, Style
 
+
 class Player:
     """ Creates an instance of Player """
-    def __init__(self, letter): 
-       """ letter is the name that the player picks their name """
-       self.letter = letter
-    
-    def get_move(self, game):  
+    def __init__(self, letter):
+        """ letter is the name that the player picks their name """
+        self.letter = letter
+
+    def get_move(self, game):
         """ All the player be able to get their next move """
         pass
-  
+
+
 class RandomComputerPlayer(Player):
     """ represent random computer player """
     def __init__(self, letter):
         super().__init__(letter)
-        
+
     def get_move(self, game):
-      # Get a random valid spot for player next move
-      square = random.choice(game.available_moves())
-      return square
-    
+        # Get a random valid spot for player next move
+        square = random.choice(game.available_moves())
+        return square
+
+
 class HumanPlayer(Player):
     """ represent human player """
     def __init__(self, letter):
         super().__init__(letter)
-        
+
     def get_move(self, game):
         valid_square = False
         valid = None
 
         while not valid_square:
 
-            square = input (Style.RESET_ALL + self.letter + "\'s turn. Input move (0-8):\n")
-            try: 
+            square = input(
+                Style.RESET_ALL + self.letter + "\'s turn. Input move (0-8):\n"
+            )
+
+            try:
                 # Check this is a correct value by try to cast
                 val = int(square)
                 if val not in game.available_moves():
@@ -42,12 +48,23 @@ class HumanPlayer(Player):
                 valid_square = True
 
             except ValueError:
-                # If that spot is not available on the board, print out "invalid" to tell player
+                # If that spot is not available on the board,
+                # print out "invalid" to tell player
                 if val < 0:
-                    print(Fore.RED + 'The number is too small. Only the 0-8 number range can be entered. Try again.')
+                    print(
+                        f"""{Fore.RED}The number is too small.
+Only the 0-8 number range can be entered.
+Try again."""
+                        )
                 elif val > 8:
-                    print(Fore.RED + 'The number is high. Only the 0-8 number range can be entered. Try again.')
-                else:   
-                    print(Fore.RED + 'This space has been occupied. Only the 0-8 number range can be entered. Try again.')
-
+                    print(
+                        f"""{Fore.RED}The number is too high.
+Only the 0-8 number range can be entered.
+Try again."""
+                        )
+                else:
+                    print(
+                        f"""{Fore.RED}This space has been occupied.
+Only the 0-8 number range can be entered.
+Try again.""")
         return val
--- run.py
+++ run.py
@@ -2,52 +2,60 @@ from player import HumanPlayer, RandomComputerPlayer
 from colorama import Fore, Back, Style
 from simple_term_menu import TerminalMenu
 
+
 class TicTacToe:
     """ Define class TicTacToe and Creates a board """
-    def __init__(self): 
-        """ Use a list of length nine to represent 3x3 board then assign an 
+    def __init__(self):
+        """ Use a list of length nine to represent 3x3 board then assign an
         index in this length nine list to each of the spaces """
         self.board = [' ' for _ in range(9)]
-        # Create variable self current winner to keep track of wether or not there is a current winner in this game. And if there is who is it?
-        self.current_winner = None 
+        # Create variable self current winner to keep track of wether or not
+        # there is a current winner in this game. And if there is who is it?
+        self.current_winner = None
 
     def greeting(self):
         print(Fore.YELLOW + 'Welcome to Tic-Tic-Toe GAME!!!')
-    
-    def instructions(self): 
+
+    def instructions(self):
         """ instruction method to explain rule """
         print(f"""{Fore.BLUE}
     Here are step-by-step how to play Tic-Tac-Toe.
 Two players, human player takes 'X', and random computer player takes 'O'.
-In with an empty three multiply three grid. This grid consists of three rows and three columns,
+In with an empty three multiply three grid.
+This grid consists of three rows and three columns,
 resulting in a total of nine cells.
-- Players take turns placing their symbol X or O in any empty cell of the grid 
+- Players take turns placing their symbol X or O in any empty cell of the grid
   by typing the number 0-8 as shown on the first board.
 - Each number representing each cell in the board.
 - The human player 'X' makes the initial move.
 - The random computer player will generate the number to move automatically.
-- The objective is to get three of your symbols in a row, either horizontally, vertically, or diagonally.
+- The objective is to get three of your symbols in a row,
+  either horizontally, vertically, or diagonally.
 - The human player to achieve this wins the game.
-- You can stop or continue to play by selecting a simple menu using the arrow up and down 
-  to enter Play Again or Quit.
-      
-      """) 
+- You can stop or continue to play by selecting a simple
+  menu using the arrow up and down to enter Play Again or Quit.
+
+      """)
+
     def print_board(self):
         """ print board method to print out the first board """
-        formatted_board_lines = ['| ' + ' | '.join(self.board[i*3:(i + 1)*3]) + ' |' for i in range(3)]
+        formatted_board_lines = ['| ' + ' | '.join(self.board[
+            i*3:(i + 1)*3]) + ' |' for i in range(3)]
         formatted_board = '\n'.join(formatted_board_lines)
         print(formatted_board)
 
     @staticmethod
     def print_board_nums():
-        """ Creates method to print out the number on the board(What number corresponds to what box) """
-        number_board = [f'{i}' for i in range (9)]
-        number_board = [number_board[i:i+3] for i  in range(0, 9, 3)] 
-        for row in number_board: 
+        """ Creates method to print out the number
+        # on the board(What number corresponds to what box) """
+        number_board = [f'{i}' for i in range(9)]
+        number_board = [number_board[i:i+3] for i in range(0, 9, 3)]
+        for row in number_board:
             print('| ' + ' | '.join(row) + ' |')
 
     def available_moves(self):
-        """ This method to create available moves(to tell player what are the available moves after they make a move """
+        """ This method to create available moves
+        (to tell player what are the available moves after they make a move """
         moves = []
         for (i, spot) in enumerate(self.board):
             if spot == ' ':
@@ -61,7 +69,8 @@ resulting in a total of nine cells.
         return self.board.count(' ')
 
     def make_move(self, square, letter):
-        """ if valid move this method will assign square to letter then make a move then return true if invalid return false"""
+        """ if valid move this method will assign square to letter
+        then make a move then return true if invalid return false"""
         if self.board[square] == ' ':
             self.board[square] = letter
             if self.winner(square, letter):
@@ -72,8 +81,8 @@ resulting in a total of nine cells.
 
     def winner(self, square, letter):
         """ This method to check all of 3 in row anywhere"""
-        row_ind = square // 3 
-        row = self.board[row_ind*3 : (row_ind + 1)*3]
+        row_ind = square // 3
+        row = self.board[row_ind*3: (row_ind + 1)*3]
         if all([spot == letter for spot in row]):
             return True
 
@@ -83,8 +92,9 @@ resulting in a total of nine cells.
 
         if all([spot == letter for spot in column]):
             return True
-        
-        # Check diagonals Only if the square is an even number (0, 2, 4, 6, 8)(These are the only moves possible to win a diagonal)
+
+        # Check diagonals Only if the square is an even number
+        # (0, 2, 4, 6, 8)(These are the only moves possible to win a diagonal)
         if square % 2 == 0:
             # Left to right diagonal
             diagonal1 = [self.board[i] for i in [0, 4, 8]]
@@ -96,15 +106,18 @@ resulting in a total of nine cells.
                 return True
             # If all of these fail
         return False
-            
-def play(game, x_player, o_player, print_game=True): 
-    """ Define the play function outside the TicTacToe class to passing in a game, an X player, and O player to print out the step to tell player """
+
+
+def play(game, x_player, o_player, print_game=True):
+    """ Define the play function outside the TicTacToe class to passing in
+    a game, an X player, and O player to print out the step to tell player """
     # Return the winner of the game(the letter)! or None for a tie
     if print_game:
         game.print_board_nums()
-    letter = 'X' 
+    letter = 'X'
 
-    # Iterate while the game still has empty squares, return the winner name using return statement to break out the loop
+    # Iterate while the game still has empty squares, return the winner name
+    # using return statement to break out the loop
     while game.empty_squares():
         if letter == 'O':
             square = o_player.get_move(game)
@@ -117,26 +130,28 @@ def play(game, x_player, o_player, print_game=True):
                 print(letter + f' makes a move to square {square}')
                 game.print_board()
                 # Empty line
-                print('') 
+                print('')
             if game.current_winner:
                 if print_game:
                     print(Fore.GREEN + letter + ' WINS !')
                 return letter
             # After player made a move, alternate letter
             # Switches player
-            letter = 'O' if letter == 'X' else 'X' 
+            letter = 'O' if letter == 'X' else 'X'
 
     if print_game:
-            print("It's a tie!")
+        print("It's a tie!")
+
 
 if __name__ == '__main__':
-    # Import human player nad random computer player from player file to the top of the page first
+    # Import human player nad random computer player
+    # from player file to the top of the page first
     options = ["Play Again", "Quit"]
 
     while True:
         x_player = HumanPlayer('X')
         o_player = RandomComputerPlayer('O')
-        t = TicTacToe() 
+        t = TicTacToe()
         t.greeting()
         t.instructions()
         play(t, x_player, o_player, print_game=True)
